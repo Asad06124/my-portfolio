9b6020c5a79bc7685b10418faddf1e5da0d5ea9e
Asad06124 <asadbalqani@gmail.com>
2025-07-23 22:03:51 +0500

    fixed

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import type { Metadata } from 'next'
 import { ContactHero } from '@/components/sections/contact-hero'
 import { ContactForm } from '@/components/sections/contact-form'
